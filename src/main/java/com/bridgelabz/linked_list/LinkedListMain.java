32cb9c51cfa7bec06c308d29a8f57799d264b318
Mrk0p <sachinwele23@gamil.com>
2023-07-09 19:22:09 +0530

    Added:create node of linked list

--- src/main/java/com/bridgelabz/linked_list/LinkedListMain.java
+++ src/main/java/com/bridgelabz/linked_list/LinkedListMain.java
@@ -1,15 +1,72 @@
 package com.bridgelabz.linked_list;
 
-import java.util.LinkedList;
-
 public class LinkedListMain {
     public static void main(String[] args) {
+
         LinkedList<Integer> linkedList1 = new LinkedList<>();
 
         linkedList1.add(56);
         linkedList1.add(30);
         linkedList1.add(70);
 
-        System.out.println("Linked List => " + linkedList1);
+        linkedList1.display();
+    }
+
+    public static class LinkedList<E> {
+        Node<E> head ;
+
+        Node<E> tail ;
+
+        public void add(E data){
+            Node<E> newNode = new Node<>(data);
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                newNode.setNext(head);
+                head = newNode;
+            }
+        }
+
+        public void display() {
+            Node<E> temp = head;
+            if(head == null){
+                System.out.println("Linked List is empty");
+            }
+            while(temp != null){
+                System.out.print(temp.getData() + " ");
+                temp = temp.getNext();
+            }
+        }
+    }
+
+    public static class Node<E> {
+        private E data ;
+
+        private Node<E> next ;
+
+        public Node() {
+        }
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
